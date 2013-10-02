28817bfd44fe71d83a33c5296b9fee50e2b1a28a
ashley williams <heyashleyashley@gmail.com>
2013-10-02 10:56:06 -0400

    ex2-6 document functions

--- ch02/combined.erl
+++ ch02/combined.erl
@@ -3,4 +3,5 @@
 -import(drop, [fall_velocity/1]).
 -import(convert, [mps_to_mph/1]).
 
+%% uses logic from drop and convert modules
 height_to_mph(Meters) -> convert:mps_to_mph(drop:fall_velocity(Meters)).
