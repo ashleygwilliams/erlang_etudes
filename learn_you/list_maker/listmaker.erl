9f2e4e1538624c662cf2312763f2c3523736d57f
ashley williams <heyashleyashley@gmail.com>
2013-10-08 16:03:05 -0400

    add spec for listmaker exercise

--- learn_you/list_maker/listmaker.erl
+++ learn_you/list_maker/listmaker.erl
@@ -1,5 +1,8 @@
 -module(listmaker).
 -export([add_to_head/2, add_to_tail/2]).
 
+-spec(add_to_head(any(), list()) -> list()).
 add_to_head(Item, List) -> [Item | List].
+
+-spec(add_to_head(any(), list()) -> list()).
 add_to_tail(Item, List) -> lists:reverse([ Item | lists:reverse(List) ]).
