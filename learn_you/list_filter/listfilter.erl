3e8c615152a9f8bad3a1ea685fe1ed3676695975
ashley williams <heyashleyashley@gmail.com>
2013-10-08 19:22:51 -0400

    add specs for listfilter

--- learn_you/list_filter/listfilter.erl
+++ learn_you/list_filter/listfilter.erl
@@ -1,8 +1,11 @@
 -module(listfilter).
 -export([filter_evens/1, filter_odds/1, filter_factor/2]).
 
+-spec(filter_evens(list()) -> list()).
 filter_evens(List) -> [ N || N <- List, N rem 2=:=0].
 
+-spec(filter_odds(list()) -> list()).
 filter_odds(List) -> [ N || N <- List, N rem 2=/=0].
 
+-spec(filter_odds(list(), number()) -> list()).
 filter_factor(List, Factor) -> [ N || N <- List, N rem Factor=:=0].
