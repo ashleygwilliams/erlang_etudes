26e9d3af68d865c31ec0af14a4b4b602ed7bf9f4
ashley williams <heyashleyashley@gmail.com>
2013-10-08 16:56:58 -0400

    start add_to_tail_recursive solution

--- learn_you/list_maker/listmaker.erl
+++ learn_you/list_maker/listmaker.erl
@@ -1,8 +1,17 @@
 -module(listmaker).
--export([add_to_head/2, add_to_tail/2]).
+-export([add_to_head/2, add_to_tail/2, add_to_tail_recursive/2]).
+-import(flatten, [flatten/1]).
 
 -spec(add_to_head(any(), list()) -> list()).
 add_to_head(Item, List) -> [Item | List].
 
--spec(add_to_head(any(), list()) -> list()).
+-spec(add_to_tail(any(), list()) -> list()).
 add_to_tail(Item, List) -> lists:reverse([ Item | lists:reverse(List) ]).
+
+-spec(add_to_tail_recursive(any(), list()) -> list()).
+
+add_to_tail_recursive(Item, []) -> lists:reverse(Item);
+
+add_to_tail_recursive(Item, List) -> add_to_tail_recursive( flatten([Item |  [ hd(List) | [] ]]), tl(List)).
+
+
