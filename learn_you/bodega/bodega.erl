18c9b526215f5b7755537b388609e7c35c736595
ashley williams <heyashleyashley@gmail.com>
2013-10-08 19:49:30 -0400

    add comments and specs to bodega

--- learn_you/bodega/bodega.erl
+++ learn_you/bodega/bodega.erl
@@ -2,8 +2,11 @@
 -export([total/1]).
 
 %calculates total based on items in a list and their price (with tax added by eper function)
+-spec(total(list() -> number()).
 total(Items) -> total(tax(Items), 0).
+%helper that uses tail recursion to sum the prices
 total([H|T], Sum) -> total(T, Sum+H);
+%helper returns the sum when all the items have been added
 total([], Sum) -> Sum.
 
 %% helper function that adds tax
