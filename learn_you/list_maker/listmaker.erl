5d2dd702fdfc800a1f674f3399cd39f9cd7de7cb
ashley williams <heyashleyashley@gmail.com>
2013-10-08 17:22:00 -0400

    add comments

--- learn_you/list_maker/listmaker.erl
+++ learn_you/list_maker/listmaker.erl
@@ -2,16 +2,22 @@
 -export([add_to_head/2, add_to_tail/2, add_to_tail_recursive/2]).
 -import(flatten, [flatten/1]).
 
+%% add a non-list item to the beginning of a list
 -spec(add_to_head(any(), list()) -> list()).
 add_to_head(Item, List) -> [Item | List].
 
+%% add a non-list item to the end of a list
 -spec(add_to_tail(any(), list()) -> list()).
 add_to_tail(Item, List) -> lists:reverse([ Item | lists:reverse(List) ]).
 
+%% add a non-list item to the end of a list, recursively
 -spec(add_to_tail_recursive(any(), list()) -> list()).
+add_to_tail_recursive(Item, List) -> add_to_tail_recursive(Item, lists:reverse(List), poop).
 
-add_to_tail_recursive(Item, []) -> lists:reverse(Item);
+%% helper to first revese the string received
+add_to_tail_recursive(Item, [], poop) -> lists:reverse(Item);
 
-add_to_tail_recursive(Item, List) -> add_to_tail_recursive( flatten([Item |  [ hd(List) | [] ]]), tl(List)).
+%% then build the list with the item to add, backwards
+add_to_tail_recursive(Item, List, poop) -> add_to_tail_recursive( flatten([Item |  [ hd(List) | [] ]]), tl(List), poop).
 
 
