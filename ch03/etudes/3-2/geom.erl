1c9cc60ec279b757bf818e4cf57dabc706fef65f
ashley williams <heyashleyashley@gmail.com>
2013-10-09 15:34:03 -0400

    etudes 3-2

---
+++ ch03/etudes/3-2/geom.erl
@@ -0,0 +1,17 @@
+%% @author Ashley Williams <ashley@flatironschool.com> [http://heyashleyashley.com]
+%% @doc function to calculate the area of a shape using height and width
+%% @reference from <a href="http://chimera.labs.oreilly.com/books/1234000000726/ch02.html#CH02-ET01"> Etudes for Erlang </a>
+
+-module(geom).
+-export([area/3]).
+
+%% @doc a function that takes height and width and multiplies them together to calculate and return an area
+-spec(area(atom(), number(), number()) -> number()).
+area(rectangle, Height, Width) when Height > 0, Width > 0 ->
+  Height * Width;
+
+area(triangle, Height, Width)  when Height > 0, Width > 0 -> 
+  Height * Width * 0.5;
+
+area(ellipse, RadiusA, RadiusB)  when RadiusA > 0, RadiusB > 0
+  -> RadiusA * RadiusB * math:pi().
